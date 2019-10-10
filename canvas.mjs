beb15848d424e0d6851fd714b41474a545250c8b
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-10-10 08:50:42 -0400

    buffer to string to json

--- canvas.mjs
+++ canvas.mjs
@@ -1,48 +1,25 @@
 import CONFIGS from './configs.json'
 import https from 'https'
+import { StringDecoder } from 'string_decoder'
 
 class CanvasService {
   constructor() {
-    
+    this.data = []
   }
 
   getGrades() {
     https.get(CONFIGS.canvas.baseUrl + 'users/2359/courses', { headers: { Authorization: CONFIGS.canvas.auth } }, (res) => {
-      console.log('statusCode:', res.statusCode)
-      console.log('headers:', res.headers)
-
+      // console.log('statusCode:', res.statusCode)
+      // console.log('headers:', res.headers)
       res.on('data', (d) => {
-        process.stdout.write(d)
+        const decoder = new StringDecoder('utf8')
+        const dataString = decoder.write(d)
+        this.data = JSON.parse(dataString)
+        console.log(this.data[0].name)
       })
-
     }).on('error', (e) => {
       console.error(e)
     })
-
-    // const options = {
-    //   hostname: CONFIGS.canvas.baseUrl,
-    //   port: 443,
-    //   path: 'users/2359/courses',
-    //   method: 'GET',
-    //   headers: {
-    //     'Authorization': CONFIGS.canvas.auth
-    //   }
-    // }
-    // const req = https.request(options, (res) => {
-    //   console.log(`STATUS: ${res.statusCode}`)
-    //   console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
-    //   res.setEncoding('utf8')
-    //   res.on('data', (chunk) => {
-    //     console.log(`BODY: ${chunk}`)
-    //   })
-    //   res.on('end', () => {
-    //     console.log('No more data in response.')
-    //   })
-    // })
-    // req.on('error', (e) => {
-    //   console.error(`problem with request: ${e.message}`)
-    // })
-    // req.end()
   }
 
   test() {
