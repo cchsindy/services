f8ae59c4cfb570dc1cd010290bc1c3480afd336c
bradocchs <bradspencer@covenantchristian.org>
2019-10-10 22:38:31 -0400

    fix merge error

--- canvas.mjs
+++ canvas.mjs
@@ -17,23 +17,6 @@ class CanvasService {
     } catch(e) {
       console.log(e)
     }
-  getGrades(userId) {
-    const endpoint = `users/${userId}/courses`
-    const qs = '?include[]=total_scores&include[]=sections&per_page=100'
-    https.get(CONFIGS.canvas.baseUrl.concat(endpoint, qs),
-      { headers: { Authorization: CONFIGS.canvas.auth } },
-      (res) => {
-      let chunks = []
-      res.on('data', (d) => {
-        chunks.push(d)
-      })
-      res.on('end', () => {
-        this.data = JSON.parse(Buffer.concat(chunks))
-        console.log(this.data[0])
-      })
-    }).on('error', (e) => {
-      console.error(e)
-    })
   }
 
   test() {
