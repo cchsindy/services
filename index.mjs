b5bd5f60d40324bf82d3b5996067094992c9c92d
bradocchs <bradspencer@covenantchristian.org>
2019-12-03 22:28:05 -0500

    add spaces service

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
 export { CanvasService } from './canvas.mjs'
+export { SpacesService } from './spaces.mjs'
 export { SparkpostService } from './sparkpost.mjs'
 export { VNNService } from './vnn.mjs'
---
+++ spaces.mjs
@@ -0,0 +1,20 @@
+import { request } from './helpers.mjs'
+
+class SpacesService {
+  constructor() {
+  }
+
+  async getFileList(dir) {
+    try {
+      const url = 'https://covenant.nyc3.digitaloceanspaces.com/'
+      const data = await request(url, {}, 'text')
+      const files = Array.from(data.matchAll(RegExp('library-tv/[^<]*\.jpg', 'g')), m => m[0])
+      return files
+    } catch(e) {
+      console.log(e)
+      return null
+    }
+  }
+}
+
+export { SpacesService }
