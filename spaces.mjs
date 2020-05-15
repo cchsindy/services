9b8c275f375b931937135084f8ddcae486715287
Brad Spencer <bradspencer@Brads-Mac-mini.attlocal.net>
2020-05-15 10:49:28 -0400

    note to switch to AWS

--- spaces.mjs
+++ spaces.mjs
@@ -1,16 +1,19 @@
 import { request } from './helpers.mjs'
 
 class SpacesService {
-  constructor() {
-  }
+  constructor() {}
 
   async getFileList(dir) {
+    // switch to AWS SDK instead
     try {
       const url = 'https://covenant.nyc3.digitaloceanspaces.com/'
       const data = await request(url, {}, 'text')
-      const files = Array.from(data.matchAll(RegExp(dir.concat('/[^<]*\.jpg'), 'g')), m => m[0])
+      const files = Array.from(
+        data.matchAll(RegExp(dir.concat('/[^<]*.jpg'), 'g')),
+        (m) => m[0]
+      )
       return files
-    } catch(e) {
+    } catch (e) {
       console.log(e)
       return null
     }
